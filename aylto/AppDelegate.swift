473e575144d9cfcdbdc16e7c5c44ef7be80038a3
André Tosin <andre_tosin@hotmail.com>
2019-11-22 15:27:17 -0300

    Add firebase initial setup

--- aylto/AppDelegate.swift
+++ aylto/AppDelegate.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -32,4 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
+
+
+
 
