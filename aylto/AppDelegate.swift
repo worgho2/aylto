158a1df87bc4588a2a77019de13202a0930a76a5
André Tosin <andre_tosin@hotmail.com>
2019-11-28 14:50:22 -0300

    Add some missing imports

--- aylto/AppDelegate.swift
+++ aylto/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 
 @UIApplicationMain
@@ -18,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        let db = Firestore.firestore()
         return true
     }
 
