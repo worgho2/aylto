916dbfa810bca0e3df657edde41294a8a667a08f
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-11-28 17:23:08 -0300

    Create profile/ user card

---
+++ aylto/Controllers/CreateCardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateCardViewController.swift
+//  aylto
+//
+//  Created by Lary Tertuliano on 27/11/19.
+//  Copyright © 2019 Gabriel Taques. All rights reserved.
+//
+
+import UIKit
+
+class CreateCardViewController: UIViewController {
+    
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilePicture.makeRounded()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ aylto/Controllers/UIView.swift
@@ -0,0 +1,19 @@
+//
+//  UIView.swift
+//  aylto
+//
+//  Created by Lary Tertuliano on 28/11/19.
+//  Copyright © 2019 Gabriel Taques. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func makeRounded() {
+        self.layer.cornerRadius = self.frame.height / 2.0
+        self.layer.masksToBounds = true
+        self.layer.borderWidth = 5.0
+    }
+    
+}
