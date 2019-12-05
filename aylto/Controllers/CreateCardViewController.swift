b0e2292dbdcf87e6de54ed07e27f7d03e40c9d18
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-12-05 16:40:29 -0300

    Revert "Create card"
    This reverts commit 472690de58a4079d202ce09ad8b21aa0794f79fe.

--- aylto/Controllers/CreateCardViewController.swift
+++ aylto/Controllers/CreateCardViewController.swift
@@ -8,57 +8,15 @@
 
 import UIKit
 
-class CardViewController: UIViewController,  UICollectionViewDelegate, UICollectionViewDataSource {
-  
-    
+class CreateCardViewController: UIViewController {
 
-    @IBOutlet var disciplinesCollection: UICollectionView!
-    
-    
-    @IBOutlet weak var interestsCollection: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        disciplinesCollection.delegate = self
-        disciplinesCollection.dataSource =  self
-        
-        interestsCollection.delegate = self
-        interestsCollection.dataSource = self
-        
         // Do any additional setup after loading the view.
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == disciplinesCollection {
-            return 10
-        } else {
-            return 5
-        }
-      }
-      
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == disciplinesCollection {
-            return setDisciplineCell(collectionView, indexPath)
-        } else {
-//            fatalError("Método não implementado")
-            return setInterestCell(collectionView, indexPath)
-        }
-      }
-    
-    private func setDisciplineCell(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> UICollectionViewCell {
-        let  cell = collectionView.dequeueReusableCell(withReuseIdentifier: "disciplinesCell", for: indexPath) as! DisciplinesCollectionViewCell
-        
-        return cell
-    }
-    
-    private func setInterestCell(_ collectionView: UICollectionView, _ indexPath: IndexPath) -> UICollectionViewCell {
-          let  cell = collectionView.dequeueReusableCell(withReuseIdentifier: "interestsCell", for: indexPath) as! InterestsCollectionViewCell
-          
-          return cell
-      }
+
     /*
     // MARK: - Navigation
 
