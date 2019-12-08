a78de78a3dc5207e92916eebcd4372a33d0dee9d
Gabriel Taques <gabrielpctaques@gmail.com>
2019-12-08 13:02:02 -0300

    Coloquei gelinho na xib dos participantes do evento

---
+++ aylto/Views/EventAlbum/ParticipantCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ParticipantCollectionViewCell.swift
+//  aylto
+//
+//  Created by Gabriel Taques on 08/12/19.
+//  Copyright © 2019 Gabriel Taques. All rights reserved.
+//
+
+import UIKit
+
+class ParticipantCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iceCoverImageView: UIImageView!
+    @IBOutlet weak var participantImageView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
